a1b0955056647e687f8581dba8ae5b39e75be197
Ivan Alagenchev <alagenchev@gmail.com>
2014-03-02 22:21:16 -0800

    adding authorization

--- src/main/java/springconfig/security/SecurityConfig.java
+++ src/main/java/springconfig/security/SecurityConfig.java
@@ -3,18 +3,30 @@ package springconfig.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
 @EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth)
 			throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("password")
-				.roles("USER");
+				.roles("USER", "TELLER");
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+	        .authorizeRequests()                                                      
+	            .antMatchers("/**").hasRole("USER")                            
+	            .anyRequest().authenticated()                                                   
+	            .and()
+	        // ...
+	        .formLogin();
 	}
 }
